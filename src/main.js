995211750984128a3b297f93ee99cadd8091a2fc
Danielle Meerburg <danielle_meerburg@live.nl>
2019-01-07 21:59:18 +0100

    added components for list and input field

--- src/main.js
+++ src/main.js
@@ -1,12 +1,38 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+Vue.component("message", {
+  template: `
+  <div class = "message">
+    <h1>{{ message }}</h1>
+    <input v-model="message">
+    <button v-on:click="">Add le kitteh</button>
+  </div>
+  `,
+  data(){
+    return {
+    message: "Sokken"
+  }
+}
+})
 
-Vue.config.productionTip = false;
+Vue.component("ilcats", {
+  props:["message"],
+  template: `
+  <div class = "ilcats">
+  <p>I love cats</p>
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+  <ul>
+    <li v-for="cat in cats">{{cat}}</li>
+    <li {{message.input}}</li>
+    
+  </ul>
+  </div>
+  `,
+  data(){
+    return {
+      cats:["Persian", "Spynx", "Ragdoll", "Vulisbakkie Sjaakie"]
+    }
+     
+}})
+
+var app = new Vue({
+  el:"#app"
+  })
