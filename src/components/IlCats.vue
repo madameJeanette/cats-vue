395f2245c739c2c0bb48e6e6dfbb7babd8e4db54
Danielle Meerburg <danielle_meerburg@live.nl>
2019-01-10 00:10:24 +0100

    adding and removing with local storage

--- src/components/IlCats.vue
+++ src/components/IlCats.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="ilcats">
     <h1>Crazy cat lady PokeDex</h1>
-    <img src="../assets/animals-cats-cute-45170.jpg" alt="Five cute baby cats in a row" width="800" height= auto>
+    <img
+      src="../assets/animals-cats-cute-45170.jpg"
+      alt="Five cute baby cats in a row"
+      width="800"
+      height="auto"
+    >
     <h3>{{ message }}</h3>
     <input type="text" v-model="message">
-    <button v-on:click="addCat()" class="btn btn-primary btn-sm">Add le kitteh</button>
+    <button v-on:click="addCat()">Add le kitteh</button>
+    <button @click="removeCat(n)">Remove</button>
     <p>I love cats</p>
 
     <ul>
-      <li v-for="cat in cats">{{cat}}</li>
+      <li v-for="(cat, n) in cats">{{cat}}</li>
     </ul>
   </div>
 </template>
@@ -17,10 +23,20 @@ export default {
   name: "IlCats",
   data() {
     return {
-      message: " ",
+      message: null,
       cats: ["Persian", "Spynx", "Ragdoll", "Vulisbakkie Sjaakie"]
     };
   },
+  mounted() {
+    if (localStorage.getItem("cats")) {
+      try {
+        this.cats = JSON.parse(localStorage.getItem("cats"));
+      } catch (e) {
+        localStorage.removeItem("cats");
+      }
+    }
+  },
+
   methods: {
     addCat: function() {
       var newCat = this.message.trim();
@@ -29,6 +45,15 @@ export default {
       }
       this.cats.push(newCat);
       this.message = "";
+      this.saveCats();
+    },
+    removeCat(x) {
+      this.cats.splice(x, 1);
+      this.saveCats();
+    },
+    saveCats() {
+      const parsed = JSON.stringify(this.cats);
+      localStorage.setItem("cats", parsed);
     }
   }
 };
