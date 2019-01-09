90d9992fe7daa4cdec785bc8c5148ee5791bd665
Danielle Meerburg <danielle_meerburg@live.nl>
2019-01-09 16:07:54 +0100

    Add input to list items

--- src/main.js
+++ src/main.js
@@ -1,38 +1,34 @@
-Vue.component("message", {
-  template: `
-  <div class = "message">
-    <h1>{{ message }}</h1>
-    <input type = "text" v-model="message" placeholder="add new Cat here"/>
-    <button v-on:click="addCat()" class="btn btn-primary btn-sm">Add le kitteh</button>
-  </div>
-  `,
-  data(){
-    return {
-    message: "Sokken"
-  }
-}
-})
-
 Vue.component("ilcats", {
-  props:["message"],
   template: `
+  
   <div class = "ilcats">
+  <h1>{{ message }}</h1>
+  <input type = "text" v-model="message">
+  <button v-on:click="addCat()" class="btn btn-primary btn-sm">Add le kitteh</button>
   <p>I love cats</p>
 
   <ul>
     <li v-for="cat in cats">{{cat}}</li>
-    <li {{message.input}}</li>
-    
-  </ul>
+      </ul>
   </div>
   `,
-  data(){
+  data() {
     return {
-      cats:["Persian", "Spynx", "Ragdoll", "Vulisbakkie Sjaakie"]
+      message: " ",
+      cats: ["Persian", "Spynx", "Ragdoll", "Vulisbakkie Sjaakie"]
+    };
+  },
+  methods: {
+    addCat: function() {
+      var newCat = this.message.trim();
+      if (!newCat) {
+        return;
+      }
+      this.cats.push(newCat);
+      this.message = "";
     }
-     
-}})
-
-var app = new Vue({
-  el:"#app"
-  })
+  }
+}),
+  new Vue({
+    el: "#app"
+  });
